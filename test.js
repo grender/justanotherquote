cf68b7332b2613c469ddd49605c101aafb07e2bd
U-grender-laptop\grender <grender@grender-laptop.(none)>
2011-03-11 17:40:33 +1000

    Normal test in test.js

--- test.js
+++ test.js
@@ -1,1 +1,13 @@
-console.log(new Buffer("Hello", 'binary').toString('base64'));
+var http = require('http');
+var haml = require('haml');
+
+http.createServer(serverMain).listen(8807);
+function serverMain(request, response){
+        response.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
+		var t=new Buffer("Hello", 'binary').toString('base64');
+		console.log(t);
+		response.end(t);
+		return;
+		}
